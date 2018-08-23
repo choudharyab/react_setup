15d4df79a2227f993ae227723b1d54049cbfd822
Anup Choudhary <anup.choudhary@tecture.in>
2018-08-23 12:46:56 +0530

    added files

---
+++ react_setup/src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App= () => {
+	 return <div>Hi i am Anup choudhary</div>;
+}
+
+
+
+ReactDOM.render(< App /> ,document.querySelector('.container'));
